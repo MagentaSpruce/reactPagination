8bf81686984bdd3a3193f5fdae5d34bcf55469f9
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-24 20:05:16 -0400

    Updates

---
+++ precache-manifest.ede445ea60c562690f250d625977f789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c693d37777a5dd779ad1cc8d5aec90",
+    "url": "/reactPagination/index.html"
+  },
+  {
+    "revision": "2aea95c4b5ad74ea97b7",
+    "url": "/reactPagination/static/css/main.4e4b0299.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/reactPagination/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPagination/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aea95c4b5ad74ea97b7",
+    "url": "/reactPagination/static/js/main.3df5e558.chunk.js"
+  },
+  {
+    "revision": "24943560ddbfd79bb78c",
+    "url": "/reactPagination/static/js/runtime-main.49d325e1.js"
+  }
+]);
